3405c04c2bf1fed5dfa870c7daade58d7f9bd46e
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-03-25 16:04:15 +0600

    header nav modify

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,21 +5,27 @@ import './Cart.css'
 const Cart = ({gun}) => {
     // console.log(gun.name)
     const {name, img, price, action, bullet} = gun;
+
+    const haldleAddToCart =() =>{
+        console.log('AddCard')
+    }
     return (
         <div className='guns-cart'>
             <div className="card Larger shadow">
-                <img src={img} alt="" className="card-img-top card-img"/>
+                <div>
+                     <img src={img} alt="" className="card-img-top card-img p-2"/>
+                </div>
                 <div className="card-body body-cart">
-                    <h5 className="card-title">{name}</h5>
+                    <h4 className="card-title">{name}</h4>
                     <p className="card-text">
-                    Price: {price} <br />
+                    <span className='fs-4 fw-bold'>Price: {price}</span> <br />
                     Action: {action} <br />
                     Bullet: {bullet}
                     </p>
                     
                 </div>
                 <div className='footer-card'>
-                     <button className='card-btn py-2 w-100'><small className='me-2 fs-5 fw-bold'>Add to Card</small><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></button>  
+                     <button className='card-btn py-2 w-100' onClick={haldleAddToCart}><small className='me-2 fs-5 fw-bold'>Add to Card</small><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></button>  
                 </div>
             </div>    
         </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import icon from "../images/gunlogo.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Header.css'
 
 const Header = () => {
     return (
         <div className='fixed-top'>
-           <nav className="navbar navbar-dark bg-dark py-3">
-                <div className="container-fluid ">
-                    <a className="navbar-brand" href="#">
-                       <img src={icon} alt="" width="40" height="29" className="d-inline-block align-text-top me-3 ms-5 rounded"/>
-                        <span className='fs-4 fw-bold'>Guns Stor</span>
-                    </a>
+            <nav className="navbar navbar-dark bg-dark py-3">
+                <div className="container-fluid">
+                    <div>
+                         <a className="navbar-brand" href="#">
+                        <img src={icon} alt="" width="40" height="29" className="d-inline-block align-text-top me-3 ms-5 rounded"/>
+                            <span className='fs-4 fw-bold'>Guns Stor</span>
+                        </a>
+                    </div>
+                    <div>
+                        <ul class="navbar-nav me-5">
+                            <li className="nav-item">
+                            <div className='countBtn'>
+                                <button type="button" className="btn btn-dark position-relative"><FontAwesomeIcon className='fs-4' icon={faShoppingCart}></FontAwesomeIcon>
+                                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                            0+
+                                        <span className="visually-hidden">unread messages</span>
+                                        </span>
+                                </button>
+                            </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </div>
